723fb81a4c5316cfc451d7a34b1d9056da8e4e06
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-19 18:47:55 -0300

    📦 FEAT: IUserRepository create interface

---
+++ backend/src/app/data/interfaces/repositories/user.ts
@@ -0,0 +1,14 @@
+import { UserEntity } from "src/app/domain/entities/user";
+import { Either } from "src/app/shared/either";
+
+interface UpdateUserProps {
+    email?: string
+    password?: string
+}
+
+export interface IUserRepository {
+    create(user: UserEntity) : Promise<Either<Error, UserEntity>>
+    findById(id: string) : Promise<Either<Error, UserEntity>>
+    findByEmail(email: string): Promise<Either<Error, UserEntity>>
+    update(id: string, update: UpdateUserProps): Promise<Either<Error, void>>
+}
