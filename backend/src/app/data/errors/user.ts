bd9dae5e3d08a8fb16452161de81ce864e949c37
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-19 19:39:31 -0300

    📦 FEAT: add new error UserUpdateAllFieldsIsEmptyError

--- backend/src/app/data/errors/user.ts
+++ backend/src/app/data/errors/user.ts
@@ -5,3 +5,9 @@ export class UserEmailAlreadyExistsError extends ErrorBase {
         super("O email de usuário já existe!", 409)
     }
 }
+
+export class UserUpdateAllFieldsIsEmptyError extends ErrorBase {
+    constructor() {
+        super("O campo de email ou o campo de id devem existir!", 400)
+    }
+}
