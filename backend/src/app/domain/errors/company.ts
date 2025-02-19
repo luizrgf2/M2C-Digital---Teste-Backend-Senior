b528f369ff33aa086affe0f3460b21acd3ccf41d
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-19 16:43:31 -0300

    🛠  REFACTOR: fix grammar erros

--- backend/src/app/domain/errors/company.ts
+++ backend/src/app/domain/errors/company.ts
@@ -2,12 +2,12 @@ import { ErrorBase } from "src/app/shared/errorBase";
 
 export class CompanyInvalidDocumentError extends ErrorBase {
     constructor(minLength: number = 4, maxLength: number = 400) {
-        super(`O documento deve ter de ${minLength} ate ${maxLength} caracteres`, 400)
+        super(`O documento deve ter entre ${minLength} e ${maxLength} caracteres.`, 400);
     }
 }
 
 export class CompanyInvalidNameError extends ErrorBase {
     constructor(minLength: number = 4, maxLength: number = 100) {
-        super(`O nome da company deve ter de ${minLength} ate ${maxLength} caracteres`, 400)
+        super(`O nome da empresa deve ter entre ${minLength} e ${maxLength} caracteres.`, 400);
     }
 }
--- backend/src/app/domain/errors/user.ts
+++ backend/src/app/domain/errors/user.ts
@@ -2,18 +2,18 @@ import { ErrorBase } from "src/app/shared/errorBase";
 
 export class UserInvalidEmailError extends ErrorBase {
     constructor() {
-        super("O email não é válido", 400)
+        super("O e-mail informado não é válido.", 400);
     }
 }
 
 export class UserInvalidPasswordLengthError extends ErrorBase {
     constructor(minLength: number = 8, maxLength: number = 30) {
-        super(`A senha deve ter de ${minLength} ate ${maxLength} caracteres`, 400)
+        super(`A senha deve ter entre ${minLength} e ${maxLength} caracteres.`, 400);
     }
 }
 
 export class UserInvalidUpperCaseLetterError extends ErrorBase {
     constructor() {
-        super("Deve ter pelo menos uma letra maiúscula", 400)
+        super("A senha deve conter pelo menos uma letra maiúscula.", 400);
     }
 }
