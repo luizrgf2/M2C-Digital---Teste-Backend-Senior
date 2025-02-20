33b9c86ca11114880d69b601a3dbe15c3f7ccbed
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-20 09:25:45 -0300

    📦 FEAT: create company repository interface

---
+++ backend/src/core/data/interfaces/repositories/company.ts
@@ -0,0 +1,26 @@
+import { CompanyEntity } from "src/core/domain/entities/company";
+import { Either } from "src/core/shared/either";
+import { ErrorBase } from "src/core/shared/errorBase";
+
+export interface UpdateCompanyProps {
+    name?: string
+    document?: string
+}
+
+export interface PaginationCompanyProps {
+    size: number,
+    skip: number
+}
+
+export interface PaginationCompanyOutput {
+    companies: CompanyEntity[],
+    count: number
+}
+
+export interface ICompanyRepository {
+    create(company: CompanyEntity) : Promise<Either<ErrorBase, CompanyEntity>>
+    findById(id: string) : Promise<Either<ErrorBase, CompanyEntity>>
+    findAll(pagination: PaginationCompanyProps): Promise<Either<ErrorBase, PaginationCompanyOutput>>
+    update(id: string, update: UpdateCompanyProps): Promise<Either<ErrorBase, CompanyEntity>>
+    delete(id: string): Promise<Either<ErrorBase, void>>
+}
