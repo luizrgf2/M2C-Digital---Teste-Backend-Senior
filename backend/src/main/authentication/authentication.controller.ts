47f6bc12d400a6edc7748b429128ca2c158d682d
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-20 08:05:36 -0300

    🛠  REFACTOR: signIn route now is public

--- backend/src/main/authentication/authentication.controller.ts
+++ backend/src/main/authentication/authentication.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import {  SignInDto } from './dto/signIn.dto';
+import { Public } from './authentication.guard';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
+  @Public()
   @Post()
   async signIn(@Body() signIn: SignInDto) {
     return this.authenticationService.signIn(signIn)
