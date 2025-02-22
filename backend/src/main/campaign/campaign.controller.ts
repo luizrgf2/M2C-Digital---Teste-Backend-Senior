7b55f06e657be2bc1b7639f869c319fcb80e63d9
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-22 12:47:08 -0300

    📦 FEAT: add suport to send files to creation of campaign

--- backend/src/main/campaign/campaign.controller.ts
+++ backend/src/main/campaign/campaign.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put, UseInterceptors, UploadedFile, HttpException } from '@nestjs/common';
 import { CampaignService } from './campaign.service';
 import { CreateCampaignDto } from './dto/createCampaign.dto';
 import { UpdateCampaignDto } from './dto/updateCampaign.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('campaign')
 export class CampaignController {
   constructor(private readonly campaignService: CampaignService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCampaignDto) {
-    return this.campaignService.create(createCompanyDto);
+  @UseInterceptors(FileInterceptor('file'))
+  create(@UploadedFile() file: Express.Multer.File, @Body() createCompanyDto: CreateCampaignDto) {
+    if(file.mimetype !== "text/plain") throw new HttpException("O arquivo tem que ser em formato texto", 400)
+    return this.campaignService.create(createCompanyDto, file.buffer.toString());
   }
 
   @Get()
