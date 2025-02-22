a54bc0f82f24a926781f8735043b23c676a325c8
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-22 12:46:39 -0300

    📦 FEAT: add rabitMqModule

--- backend/src/main/campaign/campaign.module.ts
+++ backend/src/main/campaign/campaign.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CampaignService } from './campaign.service';
 import { CampaignController } from './campaign.controller';
 import { DatabaseModule } from 'src/infra/database/database.module';
+import { RabbitMqModule } from 'src/infra/services/rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RabbitMqModule],
   controllers: [CampaignController],
   providers: [CampaignService],
 })
