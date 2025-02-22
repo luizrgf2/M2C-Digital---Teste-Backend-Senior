ba8f6f9355b9845f78d9aa26e4ed5a3feea6041d
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-22 09:30:54 -0300

    📦 FEAT: add repo in database module CampaignRepository

--- backend/src/infra/database/database.module.ts
+++ backend/src/infra/database/database.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { UserRepository } from "./userRepositoryPrisma.service";
 import { CompanyRepository } from "./companyRepository.service";
+import { CampaignRepository } from "./campaignRepository.service";
 
 @Module({
-    providers: [PrismaService, UserRepository, CompanyRepository],
-    exports: [PrismaService, UserRepository, CompanyRepository]
+    providers: [PrismaService, UserRepository, CompanyRepository, CampaignRepository],
+    exports: [PrismaService, UserRepository, CompanyRepository, CampaignRepository]
 })
 export class DatabaseModule {}
