2148559a33d807f20410e752bf9701657eea8fe2
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-02-22 12:48:01 -0300

    📦 FEAT: add new fields in file

--- backend/src/core/data/usecases/campaign/createCampaign.ts
+++ backend/src/core/data/usecases/campaign/createCampaign.ts
@@ -2,9 +2,15 @@ import { Either, Left, Right } from "src/core/shared/either";
 import { ICampaignRepository } from "../../interfaces/repositories/campaign";
 import { CampaignEntity } from "src/core/domain/entities/campaign";
 import { ErrorBase } from "src/core/shared/errorBase";
+import { IMessageProducerService } from "../../interfaces/services/messageProducer";
+import { MessageEntity } from "src/core/domain/entities/message";
 
 export interface CreateCampaignUseCaseInput {
     name: string;
+    userId: string;
+    companyId: string;
+    message: string;
+    listNumbers: string
 }
 
 export interface CreateCampaignUseCaseOutput {
@@ -16,7 +22,8 @@ export interface CreateCampaignUseCaseOutput {
 
 export class CreateCampaignUseCase {
     constructor(
-        private readonly campaignRepository: ICampaignRepository
+        private readonly campaignRepository: ICampaignRepository,
+        private readonly messageProducer: IMessageProducerService
     ) {}
 
     private createCampaignEntity(input: CreateCampaignUseCaseInput): Either<ErrorBase, CampaignEntity> {
@@ -25,12 +32,50 @@ export class CreateCampaignUseCase {
         return campaign;
     }
 
+    createMessageToSend(message: string, campaignId: string,  phoneNumber: string): Either<ErrorBase, MessageEntity> {
+        const now = new Date()
+        const messageToSend = MessageEntity.create({
+            campaignId: campaignId,
+            createdAt: now,
+            deleted: false,
+            id: "",
+            message: message,
+            phoneNumber: phoneNumber,
+            updatedAt: now
+        })
+        if(messageToSend.left) return Left.create(messageToSend.left)
+        return Right.create(messageToSend.right)
+    }
+
+    async sendMessageToQueue(messageToSend: MessageEntity, companyId: string, userId: string): Promise<Either<ErrorBase, void>> {
+        return await this.messageProducer.sendToQueue({
+            companyId: companyId,
+            message: messageToSend,
+            userId: userId
+        })
+    }
+
+    loadPhoneNumbersByListNumbersText(listNumbersText: string): string[] {
+        const numbers = listNumbersText.split("\n")
+        numbers.pop()
+        return numbers
+    }
+
     async exec(input: CreateCampaignUseCaseInput): Promise<Either<ErrorBase, CreateCampaignUseCaseOutput>> {
         const createCampaignEntityOrError = this.createCampaignEntity(input);
         if (createCampaignEntityOrError.left) return Left.create(createCampaignEntityOrError.left);
 
         const saveCampaignOrError = await this.campaignRepository.create(createCampaignEntityOrError.right);
         if (saveCampaignOrError.left) return Left.create(saveCampaignOrError.left);
+
+        const numbersToSendToQueue = this.loadPhoneNumbersByListNumbersText(input.listNumbers)
+
+        for(const phoneNumber of numbersToSendToQueue) {
+            const messageToSendOrError = this.createMessageToSend(input.message, saveCampaignOrError.right.id, phoneNumber)
+            if(messageToSendOrError.left) continue
+            const sendMessageToQueueOrError = await this.sendMessageToQueue(messageToSendOrError.right, input.companyId, input.userId)
+            if(sendMessageToQueueOrError.left) return Left.create(sendMessageToQueueOrError.left)
+        }
 
         return Right.create({
             id: saveCampaignOrError.right.id,
